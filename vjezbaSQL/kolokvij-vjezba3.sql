1ad6e31273e5dc00f96bb2e5f41b3c28a058e5cb
Nikola Milić <nikk.mil@gmail.com>
2024-06-04 14:23:28 +0200

    Create kolokvij-vjezba3.sql
    Kreirana skripta baze3 i 4 tablice

---
+++ vjezbaSQL/kolokvij-vjezba3.sql
@@ -0,0 +1,63 @@
+use master;
+go
+drop database if exists vjezba3;
+go
+create database vjezba3;
+go
+use vjezba3;
+
+create table svekar(
+sifra int not null primary key identity(1,1),
+novcica decimal(16,8) not null,
+suknja varchar(44) not null,
+bojakose varchar(36),
+prstena int,
+narukvica int not null,
+cura int not null
+);
+
+create table cura(
+sifra int not null primary key identity(1,1),
+dukserica varchar(49),
+maraka decimal (13,7),
+drugiputa datetime,
+majica varchar(49),
+novcica decimal(15,8),
+ogrlica int not null
+);
+
+create table snasa(
+sifra int not null primary key identity(1,1),
+introvertno bit,
+kuna decimal(15,6) not null,
+eura decimal(12,9) not null,
+treciputa datetime,
+ostavljena int not null
+);
+
+create table punica(
+sifra int not null primary key identity(1,1),
+asocijalno bit,
+kratkamajica varchar(44),
+kuna decimal (13,8) not null,
+vesta varchar(32) not null,
+snasa int
+);
+
+create table ostavljena(
+sifra int not null primary key identity(1,1),
+);
+
+create table prijatelj(
+sifra int not null primary key identity(1,1),
+);
+
+create table prijatelj_brat(
+sifra int not null primary key identity(1,1),
+
+);
+
+create table brat(
+sifra int not null primary key identity(1,1),
+
+);
