457fa23dda688594147aaa86420f906d20171188
Nikola Milić <nikk.mil@gmail.com>
2024-06-08 17:09:45 +0200

    Kreiranje projekta
    Kreiran projekt UcenjeCS koji se nalazi u Solutionu Ucenje

---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 
+ * 
+ * komentar kroz više redova
+ * 
+ * 
+ */
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("tekst");
+Console.WriteLine(7);
+Console.WriteLine('T');
+Console.WriteLine(true);
+Console.WriteLine("Nikola Milić");
+Console.Write("Nik");
