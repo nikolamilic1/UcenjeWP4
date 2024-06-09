986e3104af6ef96c8b4ccff9964f7b15f377fa39
Nikola Milić <nikk.mil@gmail.com>
2024-06-09 23:36:18 +0200

    Kreirana skripta baze vjezba4
    Kreirana skripta sa jednom tablicom

---
+++ vjezbaSQL/kolokvij-vjezba4.sql
@@ -0,0 +1,19 @@
+use master;
+go
+alter database vjezba4 set single_user with rollback immediate;
+go
+drop database if exists vjezba4;
+go
+create database vjezba4 collate Croatian_CI_AS;
+go
+use vjezba4;
+
+create table punac(
+sifra int not null primary key identity(1,1),
+treciputa datetime,
+majica varchar(46),
+jmbag char(11) not null,
+novcica decimal(18,7) not null,
+maraka decimal(12,6) not null,
+ostavljen int not null
+);
