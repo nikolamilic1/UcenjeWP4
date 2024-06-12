d8efa8b86a29909c5c134d985d400e92fbb75d67
Nikola Milić <nikk.mil@gmail.com>
2024-06-12 23:22:24 +0200

    Update E03UvjetnoGrananjeIf.cs

--- CSHARP/Vjezbanje/VjezbanjeCS/E03UvjetnoGrananjeIf.cs
+++ CSHARP/Vjezbanje/VjezbanjeCS/E03UvjetnoGrananjeIf.cs
@@ -26,11 +26,31 @@ namespace VjezbanjeCS
             {
                 Console.WriteLine("1. Ušao sam u if granu");
             }
+
+            // dodatak (neobavezni dio) IF grananja je ELSE
             else
             {
                 Console.WriteLine("2. Uvjet nije zadovoljen, otišao u else granu");
             }
 
+            // uobičajena sintaksa što se tiče uvjeta
+
+            if (i > 2) // ako ne postoje vitičaste zagrade IF se odnosi samo na 1. selijedeću liniju
+                Console.WriteLine("3. Ušao u IF bez vitičastih zagrada");
+            Console.WriteLine("4. ovo se izvodi bez obzira na gornji if");
+
+            var j = 2;
+
+            // dodatni operatori & (and), && (and and), | (or), || (or or), ! (not) 
+            // *** znak | (or) piše se AltGr+W  ***
+            // *** čišćenje sintakse ide sa CTRL+K+D ***
+
+            if (i < 2 && j == 2)
+            {
+                Console.WriteLine("5. Logičko &");
+            }
+            // & provjerava oba uvjeta bez obzira što je možda prvi FALSE
+            // && ukoliko prvi uvjet bude FALSE drugi se ne provjerava
 
 
 
@@ -40,7 +60,6 @@ namespace VjezbanjeCS
 
 
 
-
-            } 
+        }
     }
 }
