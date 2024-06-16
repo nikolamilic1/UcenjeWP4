36fa5d09f31c276d61441a929cf627328054a8d7
Nikola Milić <nikk.mil@gmail.com>
2024-06-16 23:11:31 +0200

    Update kolokvij-vjezba5.sql

--- vjezbaSQL/kolokvij-vjezba5.sql
+++ vjezbaSQL/kolokvij-vjezba5.sql
@@ -7,3 +7,36 @@ go
 create database vjezba5 collate Croatian_CI_AS;
 go
 use vjezba5;
+
+create table mladic(
+sifra int not null primary key identity(1,1),
+);
+
+create table zarucnik(
+sifra int not null primary key identity(1,1),
+);
+
+create table punac(
+sifra int not null primary key identity(1,1),
+);
+
+create table punica(
+sifra int not null primary key identity(1,1),
+);
+
+create table ostavljena(
+sifra int not null primary key identity(1,1),
+);
+
+create table svekar(
+sifra int not null primary key identity(1,1),
+);
+
+create table svekar_cura(
+sifra int not null primary key identity(1,1),
+);
+
+create table cura(
+sifra int not null primary key identity(1,1),
+);
+
