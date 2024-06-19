5831e857150c90dc18dc1832b383a8a81bc49ac5
Nikola Milić <nikk.mil@gmail.com>
2024-06-19 23:28:37 +0200

    Create E01Z4.cs

---
+++ CSHARP/Vjezbanje/VjezbanjeCS/E01Z4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeCS
+{
+    internal class E01Z4
+    {
+        internal static void Izvedi()
+        {
+            // Program od korisnika učitava dvije rečenice i ispisuje jednu nakon druge u istom retku
+            Console.WriteLine("Unesi prvu rečenicu");
+
+
+
+        }
+    }
+}
