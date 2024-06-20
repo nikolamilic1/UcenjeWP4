0101b1d03cae9330cfb250a4243ecc986c34604f
Remba15 <renatokuna01@gmail.com>
2024-06-20 18:02:33 +0200

    DoWhilePetlja

---
+++ CSHARP/Ucenje/UcenjeCS/E08DoWhilePetlja.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E08DoWhilePetlja
+    {
+        internal static void Izvedi()
+        {
+            // do while izdvaja činjenica da će se on barem jednom izvesti
+
+
+            int i =0;
+             do
+            {
+                Console.WriteLine(i);
+
+            } while (i > 0);
+        }
+    }
+}
