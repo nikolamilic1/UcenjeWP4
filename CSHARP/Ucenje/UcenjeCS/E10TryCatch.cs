fd56a8840565edc41a0dde68cdfdd90d384918f0
Remba15 <renatokuna01@gmail.com>
2024-06-20 18:37:11 +0200

    Update E10TryCatch.cs

--- CSHARP/Ucenje/UcenjeCS/E10TryCatch.cs
+++ CSHARP/Ucenje/UcenjeCS/E10TryCatch.cs
@@ -8,5 +8,38 @@ namespace UcenjeCS
 {
     internal class E10TryCatch
     {
+        internal static void Izvedi()
+        {
+
+            
+
+            int i;
+            while (true)
+            {
+                Console.WriteLine("Unesi cijeli pozitivni broj manji od 100: ");
+                try
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (i > 0 && i<100)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Uneseni broj nije veći od nule ");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Niste unijeli broj");
+                }
+
+            }
+
+            // ovdje si siguran da imaš cijeli broj
+
+            Console.WriteLine("Unijeli ste broj " + i);
+
+
+
+
+        }
     }
 }
