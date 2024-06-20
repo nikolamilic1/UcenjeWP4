a711baf1813bd8777e41466d2bd1062b44a583bc
Remba15 <renatokuna01@gmail.com>
2024-06-20 18:02:08 +0200

    For EachPetlja

---
+++ CSHARP/Ucenje/UcenjeCS/E09ForEachPetlja.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E09ForEachPetlja
+    {
+        internal static void Izvedi()
+        {
+            string rijec = "osijek";
+            for (int i = 0; i < rijec.Length; i++)
+            {
+                Console.WriteLine(rijec[i]);
+            }
+
+
+
+            foreach (var znak in rijec)
+            {
+                Console.WriteLine(znak);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
