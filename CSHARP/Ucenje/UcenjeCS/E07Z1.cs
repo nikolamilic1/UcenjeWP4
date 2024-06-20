924b59b4c0e7da88028d881ef1b0d7c1806d0fba
Remba15 <renatokuna01@gmail.com>
2024-06-20 18:03:14 +0200

    Zadatak

---
+++ CSHARP/Ucenje/UcenjeCS/E07Z1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E07Z1
+    {
+        internal static void Izvedi()
+        {
+
+
+            //Console.WriteLine("Unesi broj: ");
+            //int brojOd = 1;
+            //int brojDo;
+            //brojDo = int.Parse(Console.ReadLine());
+
+            //while (brojDo > 1)
+            //{
+
+            //}
+            //while (brojOd < brojDo && br < brojDo)  
+            //{
+
+            //    Console.WriteLine(j++);
+
+            //}
+        }
+    }
+}
