a097e0814cc5a7437c1a188191590ef5758f6d21
Nikola Milić <nikk.mil@gmail.com>
2024-06-24 23:47:31 +0200

    Create Radno.cs

---
+++ CSHARP/Zadaca/ZadacaCS/Radno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadacaCS
+{
+    internal class Radno
+    {
+        internal static void Izvedi()
+        {
+            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+               // Console.Write("Row " + i + ": ");
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.Write(numbers[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
