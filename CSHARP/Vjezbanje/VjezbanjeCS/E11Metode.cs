0ef471167584f8ee2b191d01f7271e942f232118
Nikola Milić <nikk.mil@gmail.com>
2024-06-27 00:06:37 +0200

    Update E11Metode.cs

--- CSHARP/Vjezbanje/VjezbanjeCS/E11Metode.cs
+++ CSHARP/Vjezbanje/VjezbanjeCS/E11Metode.cs
@@ -12,11 +12,12 @@ namespace VjezbanjeCS
         public static void Izvedi()
         {
             // Poziv metode
-            Tip1();
-            Tip1();
+            // Tip1();
+            // Tip1();
             for (int i = 0; i < 55; i++) {
                 Tip1();
             }
+            Tip2("Edunova");
         }
         // Tip 1. Metoda ne prima parametre i ne vraća vrijednost
         // Ovdje je deklarirana, napisana
@@ -27,6 +28,12 @@ namespace VjezbanjeCS
             Console.WriteLine("Drugi red");
             Console.WriteLine("Treći red");
         }
+        // Tip 2. metoda prima parametre i ne vraća vrijednost
+        // kada ispred [static] void nema načina pristupa tada je private
 
+        static void Tip2(string ime)
+        {
+            Console.WriteLine("Hello {0}", ime);             // wp4 25.06.2024. 00:19:00
+        }
     }
 }
