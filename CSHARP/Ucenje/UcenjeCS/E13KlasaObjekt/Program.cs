f55cb4c8c1acae59da8de2e5443558ceb6c33e6c
Nikola Milić <nikk.mil@gmail.com>
2024-06-27 17:10:07 +0200

    Klasa objekt ucenje

---
+++ CSHARP/Ucenje/UcenjeCS/E13KlasaObjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E13KlasaObjekt
+{
+    public class Program
+    {
+        public static void Izvedi()
+        {
+            
+        }
+    }
+}
--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -5,9 +5,11 @@ using UcenjeCS;
 
 
 
+using UcenjeCS.E13KlasaObjekt;
 
+UcenjeCS.E13KlasaObjekt.Program.Izvedi();
 
-E12Rekurzija.Izvedi();
+//E12Rekurzija.Izvedi();
 
 
 //E11Z1.Izvedi();
