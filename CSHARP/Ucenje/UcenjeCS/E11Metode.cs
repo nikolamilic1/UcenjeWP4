fe1663099256cd5fd4829330e5089deb2be61a37
Nikola Milić <nikk.mil@gmail.com>
2024-07-03 23:06:43 +0200

    Update E11Metode.cs

--- CSHARP/Ucenje/UcenjeCS/E11Metode.cs
+++ CSHARP/Ucenje/UcenjeCS/E11Metode.cs
@@ -26,10 +26,11 @@ namespace UcenjeCS
             //Console.WriteLine("Unesi ime:");
             //Tip2(Console.ReadLine());
             //Tip2("Pero","Perić");
-            int broj = Tip3();
-            Console.WriteLine(broj);
+            //int broj = Tip3();
+            //Console.WriteLine(broj);
 
             //Tip4(1,99);
+            Console.WriteLine(Tip4(2,8));
 
             //int doBroja = Pomocno.UcitajCijeliBroj("Unesi do broja");
             //int odBroja = Pomocno.UcitajCijeliBroj("Unesi od broja");
@@ -41,42 +42,42 @@ namespace UcenjeCS
         // Tip 1. metoda ne prima parametre i ne vraća vrijednost
         // Ovdje je deklarirana, napisana
         // PRIVATE se vidi samo u trenutnoj klasi
-        private static void Tip1()
-        {
-            Console.WriteLine("Hello iz metode koja ne prima parametre i ne vraća vrijednost");
-            Console.WriteLine("Drugi red");
-            Console.WriteLine("Treći red");
-        }
+        //private static void Tip1()
+        //{
+        //    Console.WriteLine("Hello iz metode koja ne prima parametre i ne vraća vrijednost");
+        //    Console.WriteLine("Drugi red");
+        //    Console.WriteLine("Treći red");
+        //}
 
         // Tip 2. metoda prima parametre i ne vraća vrijednost
         // kada ispred [static] void nema načina pristupa tada je: private
-        static void Tip2(string ime)
-        {
-            Console.WriteLine("Hello {0}", ime);
-        }
+        //static void Tip2(string ime)
+        //{
+        //    Console.WriteLine("Hello {0}", ime);
+        //}
 
         // Potpis metode: naziv + lista parametara
         // method signature
         // method overloading
         // METOD OVERLOADING  ---ime metode može biti isto ali lista parametara mora biti različita
         // Primjer:
-        static void Tip2(string ime, string prezime)
-        {
-            Console.WriteLine("Hello {0} {1}", ime, prezime);
-        }
+        //static void Tip2(string ime, string prezime)
+        //{
+        //    Console.WriteLine("Hello {0} {1}", ime, prezime);
+        //}
 
         // Tip 3. Metoda ne prima parametre i vraća vrijednost
 
-        static int Tip3()
-        {
+        //static int Tip3()
+        //{
 
-            return new Random().Next(10, 30);
+        //    return new Random().Next(10, 30);
 
 
-            return 0;  // <- ovo je dobro staviti da ne pokazuje sintaksnu grešku
-                       //  return '7';  // return vraća vrijednost onome tko je pozvao metodu
-                       // vraća prema ASCII tablici, pogledati: https://commons.wikimedia.org/wiki/File:ASCII-Table-wide.svg
-        }
+        //    return 0;  // <- ovo je dobro staviti da ne pokazuje sintaksnu grešku
+        //               //  return '7';  // return vraća vrijednost onome tko je pozvao metodu
+        //               // vraća prema ASCII tablici, pogledati: https://commons.wikimedia.org/wiki/File:ASCII-Table-wide.svg
+        //}
 
 
         // 4. najbitniji ----  metoda prima parametre  i vraća vrijednost
@@ -90,7 +91,6 @@ namespace UcenjeCS
         /// <returns>Zbroj brojeva između dva primljena broja</returns>       
         protected static int Tip4(int odBroja, int doBroja)
         {
-
 
             int Suma = 0;
 
