67d410599b14297b8e6ef47b56b8a9d861de89e8
Nikola Milić <nikk.mil@gmail.com>
2024-07-08 14:18:54 +0200

    Edunova klasa Grupa

---
+++ CSHARP/Ucenje/UcenjeCS/Edunova/Grupa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.Edunova
+{
+    internal class Grupa
+    {
+        public int? Sifra { get; set; }
+        public string? Naziv { get; set; }
+        public Smjer? Smjer { get; set; }
+        public string? Predavac { get; set; }
+        public int? MaksimalnoPolaznika { get; set; }
+    }
+}
