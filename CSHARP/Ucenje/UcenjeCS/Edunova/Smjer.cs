cc900ff29ef75063e68695aca7160030b8158cf9
Nikola Milić <nikk.mil@gmail.com>
2024-07-08 14:13:10 +0200

    Update Smjer.cs

--- CSHARP/Ucenje/UcenjeCS/Edunova/Smjer.cs
+++ CSHARP/Ucenje/UcenjeCS/Edunova/Smjer.cs
@@ -8,5 +8,27 @@ namespace UcenjeCS.Edunova
 {
     internal class Smjer
     {
+
+        public int? Sifra { get; set; }
+        public int? Naziv { get; set; }
+        public int? Trajanje { get; set; }
+        public decimal? Cijena { get; set; }
+        public DateTime? IzvodiSeOd { get; set; }
+        public bool? Verificiran { get; set; }
+
+        // Ovaj gore prop bi u većini drugih programskih jezika (a može i ovdje) izgledao ovako:
+        private int broj;
+        public int getBroj()
+        {
+            return broj;
+        }
+        //this - u ovoj klasi
+        public void setBroj(int broj)
+        {
+            this.broj = broj;
+        }
+
+
+
     }
 }
