6a9066ba0fd180545e4f213953935d879c6a1660
Nikola Milić <nikk.mil@gmail.com>
2024-07-28 23:49:18 +0300

    Create Grupa.cs

---
+++ CSHARP/Vjezbanje/VjezbanjeCS/E18KonzolnaAplikacija/Model/Grupa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbanjeCS.E18KonzolnaAplikacija.Model
+{
+    internal class Grupa:Entitet
+    {
+        public string? Naziv { get; set; }
+        public Smjer? Smjer { get; set; }
+
+        public string? Predavac { get; set; }
+
+        public int MaxPolaznika { get; set; }
+
+        public List<Polaznik>? Polaznici { get; set; }  
+
+    }
+}
