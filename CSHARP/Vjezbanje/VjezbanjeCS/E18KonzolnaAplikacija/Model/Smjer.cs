b7c953c605a2d99919b58af982f0c3a969e76494
Nikola Milić <nikk.mil@gmail.com>
2024-07-28 20:28:30 +0300

    Update Smjer.cs
    Upisani atributi u Entitet Smjer

--- CSHARP/Vjezbanje/VjezbanjeCS/E18KonzolnaAplikacija/Model/Smjer.cs
+++ CSHARP/Vjezbanje/VjezbanjeCS/E18KonzolnaAplikacija/Model/Smjer.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace VjezbanjeCS.E18KonzolnaAplikacija.Model
 {
-    internal class Smjer
+    internal class Smjer:Entitet
     {
+        public String? Naziv { get; set; }
+        public int? Trajanje { get; set; }
+
+        public float? Cijena { get; set; }
+
+        public DateTime? IzvodiSeOd { get; set; }
+
+        public bool? Verificiran { get; set; }
     }
 }
