e5a7f0ee83ed65a7c2b8bb141c046abfe45c266a
Nikola Milić <nikk.mil@gmail.com>
2024-07-30 10:06:38 +0200

    Create Zadatak.cs

---
+++ CSHARP/Zadaca/ZadacaCS/EvidencijaRadaKonzolna/Model/Zadatak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadacaCS.EvidencijaRadaKonzolna.Model
+{
+    internal class Zadatak:Entitet
+    {
+
+        public DateTime? Pocetak { get; set; }
+
+        public DateTime? Zavrsetak { get; set; }
+
+        public Projekt? Projekt { get; set; }
+
+        public string? Opis { get; set; }
+
+        // Što sa ostalim kolonama entiteta evidencija_rada??? Jel treba u ovom slučaju napraviti  posebnu tablicu?
+        public List<Djelatnik>? Djelatnici { get; set; }
+
+    }
+}
