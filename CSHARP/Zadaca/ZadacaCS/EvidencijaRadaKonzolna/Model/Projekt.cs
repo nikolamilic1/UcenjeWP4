9877438e028706351e1a505234c504ca89acbdcf
Nikola Milić <nikk.mil@gmail.com>
2024-07-30 09:46:47 +0200

    Napravljene klase Entitet i Projekt

---
+++ CSHARP/Zadaca/ZadacaCS/EvidencijaRadaKonzolna/Model/Projekt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadacaCS.EvidencijaRadaKonzolna.Model
+{
+    internal class Projekt:Entitet
+    {
+
+        public string? Naziv { get; set; }
+        public string? Klijent { get; set; }
+
+    }
+}
