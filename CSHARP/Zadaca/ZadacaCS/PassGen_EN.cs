21e9af21abf6998eb5562966febd2b2e16d0b93d
Nikola Milić <nikk.mil@gmail.com>
2024-08-15 23:59:05 +0200

    Update PassGen_EN.cs

--- CSHARP/Zadaca/ZadacaCS/PassGen_EN.cs
+++ CSHARP/Zadaca/ZadacaCS/PassGen_EN.cs
@@ -154,6 +154,11 @@ namespace ZadacaCS
             int n = list.Count;
             while (n > 1)
             {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
 
             }
 
