6a53132b97dbbabe776a5f88aad23f27820875ae
Nikola Milić <nikk.mil@gmail.com>
2024-08-17 20:18:01 +0200

    Update LjubavniKalkulator.cs

--- CSHARP/Zadaca/ZadacaCS/LjubavniKalkulator.cs
+++ CSHARP/Zadaca/ZadacaCS/LjubavniKalkulator.cs
@@ -13,9 +13,37 @@ namespace ZadacaCS
             var ime1 = "Ivica";
             var ime2 = "Marica";
 
-            string s = ime1[0].ToString();
+            //string s = ime1[0].ToString();
+            //char c = ime1[0];
 
+            //Console.WriteLine(s);
+
+
+            string s = ime1.ToLower() + ime2.ToLower();
             Console.WriteLine(s);
+
+            int[] brojevi = new int[s.Length];
+            int u;
+            //ivicamarica
+            for (int i = 0; i < s.Length; i++)
+            {
+                /*
+                u = 0;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        u++;
+                    }
+                }
+                brojevi[i] = u;
+                */
+                brojevi[i] = s.Count(znak => znak == s[i]);
+            }
+            Console.WriteLine(string.Join(",",brojevi));
         }
+
+
+
     }
 }
