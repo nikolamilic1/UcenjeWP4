69a2ece612a6a54b35e967f70cd106f0e27a770d
Nikola Milić <nikk.mil@gmail.com>
2024-08-18 23:27:09 +0200

    Update Program.cs

--- CSHARP/Zadaca/ZadacaCS/LjubavniKalkulator/Program.cs
+++ CSHARP/Zadaca/ZadacaCS/LjubavniKalkulator/Program.cs
@@ -8,57 +8,6 @@ namespace ZadacaCS.LjubavniKalkulator
 {
     internal class Program
     {
-        public void ljubav()
-        {
-            var ime1 = "Ivica";
-            var ime2 = "Marica";
-
-            string s = ime1[0].ToString();
-            //char c = ime1[0];
-
-            Console.WriteLine(s);
-
-
-            //s = ime1.ToLower() + ime2.ToLower();
-            //Console.WriteLine(s);
-
-            //int[] brojevi = new int[s.Length];
-            //int u;
-            ////ivicamarica
-            //for (int i = 0; i < s.Length; i++)
-            //{
-            //    /*
-            //    u = 0;
-            //    for (int j = 0; j < s.Length; j++)
-            //    {
-            //        if (s[i] == s[j])
-            //        {
-            //            u++;
-            //        }
-            //    }
-            //    brojevi[i] = u;
-            //    */
-            //    brojevi[i] = s.Count(znak => znak == s[i]);
-            //}
-            //Console.WriteLine(string.Join(",", brojevi));
-
-            //Console.WriteLine("{0} i {1} se vole {2} %", ime1, ime2, jubav(brojevi));
-        }
-
-
-        //private int jubav(int[] brojevi)
-        //{
-        //    var broj = int.Parse(string.Join(",", brojevi));
-        //    if (broj <= 100)
-        //    {
-        //        return broj;
-        //    }
-
-        //    //algoritam zbrajanja
-        //    return jubav(brojevi);
-
-        //}
-
 
 
 
