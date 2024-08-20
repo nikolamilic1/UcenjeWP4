a25f7b36ddfa348a8eab96303eae6733798ae78a
Nikola <nikk.mil@gmail.com>
2024-08-20 18:34:24 +0200

    WebApi pocetak

---
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Security;
+
+namespace WebAPI.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HttpMetodeController: ControllerBase
+    {
+        
+        // ovdje počinje ruta
+        [HttpGet]
+       
+        public string Pozdravi()
+        {
+            return "Hello";
+        }
+        // ovdje završava ruta
+
+
+
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("pozdrav")]
+
+        public string Pozdravi(string s)
+        {
+            return "Hello " + s;
+        }
+        // ovdje završava ruta
+
+
+        [HttpGet]
+        [Route("Hello")]
+        public IActionResult Pozdravi(int id, string ime)
+        {
+            return Ok(new {sifra = id, naziv = ime});
+        }
+
+
+
+
+    }
+}
