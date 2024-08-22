ff7b15d00558eb391f7c91b49da45a91eea91549
Nikola Milić <nikk.mil@gmail.com>
2024-08-22 15:53:37 +0200

    PovučenoSaGita

--- CSHARP/Ucenje/WebAPI/Program.cs
+++ CSHARP/Ucenje/WebAPI/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -13,7 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(opcije => { opcije.ConfigObject.AdditionalItems.Add()});
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
