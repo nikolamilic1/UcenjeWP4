6e9d0573d8f281c4d458dadf78b2baaa3c4cba25
Nikola <nikk.mil@gmail.com>
2024-08-22 18:35:43 +0200

    Update SmjerController.cs

--- CSHARP/Ucenje/EdunovaApp/Controllers/SmjerController.cs
+++ CSHARP/Ucenje/EdunovaApp/Controllers/SmjerController.cs
@@ -1,4 +1,5 @@
 ﻿using EdunovaApp.Data;
+using EdunovaApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EdunovaApp.Controllers
@@ -26,12 +27,64 @@ namespace EdunovaApp.Controllers
         // RUTE
 
         [HttpGet]
-        public ActionResult Get()
+        public IActionResult Get()
         {
             return Ok(_context.Smjerovi);
         }
 
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public IActionResult GetBySifra(int sifra)
+        {
 
+            return Ok(_context.Smjerovi.Find(sifra));
+
+        }
+
+        [HttpPost]
+        public IActionResult Post(Smjer smjer)
+        {
+        
+            _context.Smjerovi.Add(smjer);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created, smjer);
+        
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+
+        public IActionResult Put(int sifra, Smjer smjer)
+        {
+            var smjerIzBaze = _context.Smjerovi.Find(sifra);
+
+            smjerIzBaze.Naziv = smjer.Naziv;
+            smjerIzBaze.Trajanje = smjer.Trajanje;
+            smjerIzBaze.Cijena = smjer.Cijena;
+            smjerIzBaze.IzvodiSeOd = smjer.IzvodiSeOd;
+            smjerIzBaze.Vaucer = smjer.Vaucer;
+
+            _context.Smjerovi.Update(smjerIzBaze);
+            _context.SaveChanges();
+
+            return Ok(new { poruka = "Uspješno promjenjeno" });
+
+
+        }
+
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+
+        public IActionResult Delete(int sifra)
+        {
+            var smjerIzBaze = _context.Smjerovi.Find(sifra);
+            _context.Smjerovi.Remove(smjerIzBaze);
+            _context.SaveChanges();
+            return Ok(new { poruka = "Uspješno obrisano" });
+        }
 
 
     }
