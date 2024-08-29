c4685758ffb0dd1790ab61fa8a5fa06a301a2ab3
Nikola Milić <nikk.mil@gmail.com>
2024-08-29 20:34:21 +0200

    Update HttpMetodeController.cs

--- CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -8,11 +8,20 @@ namespace WebAPI.Controllers
 [Route("api/v1/[controller]")]
     public class HttpMetodeController:ControllerBase
     {
+        
+        // Ovdje počinje ruta
         [HttpGet]
         public string Pozdravi()
         {
             return "Hello!";
         }
+        // Ovdje završava ruta
 
+        [HttpGet]
+        [Route("pozdrav")]
+        public string Pozdravi(string s)
+        {
+            return "Hello" + s;
+        }
     }
 }
