2ffaf1bf3c0783edad0570b1939c44bb07475a30
Nikola Milić <nikk.mil@gmail.com>
2024-08-29 19:59:34 +0200

    Create HttpMetodeController.cs

---
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+
+
+[ApiController]
+[Route("api/v1/[controller]")]
+    public class HttpMetodeController:ControllerBase
+    {
+
+        
+
+    }
+}
