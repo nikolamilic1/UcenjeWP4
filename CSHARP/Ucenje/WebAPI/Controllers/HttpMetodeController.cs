d940b450528791a5870b6fa99670204003871ab3
Nikola Milić <nikk.mil@gmail.com>
2024-08-29 20:28:15 +0200

    Update HttpMetodeController.cs

--- CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/HttpMetodeController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
+{ 
 
 
 [ApiController]
 [Route("api/v1/[controller]")]
     public class HttpMetodeController:ControllerBase
     {
-
-        
+        [HttpGet]
+        public string Pozdravi()
+        {
+            return "Hello!";
+        }
 
     }
 }
