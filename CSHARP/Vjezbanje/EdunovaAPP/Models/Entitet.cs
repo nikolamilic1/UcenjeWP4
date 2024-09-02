993f324439e8550c54558ad10f9b1becfc943b87
Nikola Milić <nikk.mil@gmail.com>
2024-09-02 13:33:46 +0200

    Update Entitet.cs

--- CSHARP/Vjezbanje/EdunovaAPP/Models/Entitet.cs
+++ CSHARP/Vjezbanje/EdunovaAPP/Models/Entitet.cs
@@ -1,7 +1,10 @@
-﻿namespace EdunovaAPP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdunovaAPP.Models
 {
     public abstract class Entitet
     {
+        [Key]
         public int Sifra { get; set; }
     }
 }
