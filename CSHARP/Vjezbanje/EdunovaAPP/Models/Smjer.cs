875cc6c8f00807084ca8570930d49a5e4bc08e6a
Nikola Milić <nikk.mil@gmail.com>
2024-09-02 13:38:22 +0200

    Update Smjer.cs

--- CSHARP/Vjezbanje/EdunovaAPP/Models/Smjer.cs
+++ CSHARP/Vjezbanje/EdunovaAPP/Models/Smjer.cs
@@ -2,5 +2,10 @@
 {
     public class Smjer:Entitet
     {
+        public string? Naziv { get; set; }
+        public int? Trajanje { get; set; }
+        public decimal? Cijena { get; set; }
+        public DateTime? IzvodiSeOd { get; set; }
+        public bool? Vaucer { get; set; }
     }
 }
