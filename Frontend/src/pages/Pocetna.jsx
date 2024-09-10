56a1a284cde47335af86712386a29594d52e877d
Nikola <nikk.mil@gmail.com>
2024-09-10 18:22:16 +0200

    frontend edunova

---
+++ Frontend/src/pages/Pocetna.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+
+
+
+export default function Pocetna() {
+    return (
+        <Container>
+            Dobrodošli, kasnije ćemo nešto dodati ovdje.
+        </Container>
+    )
+}
