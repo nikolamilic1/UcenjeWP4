b31fa10c1c5d274c4d4a0a58d108a8e7f50f98e0
Nikola <nikk.mil@gmail.com>
2024-09-19 18:55:57 +0200

    Update SmjerService.js

--- Frontend/src/services/SmjerService.js
+++ Frontend/src/services/SmjerService.js
@@ -1,16 +1,61 @@
-import { Await } from "react-router-dom"
 import { HttpService } from "./HttpService"
 
 
-async function get() {
+async function get(){
     return await HttpService.get('/Smjer')
     .then((odgovor)=>{
-        // console.table(odgovor.data);
+        //console.table(odgovor.data);
         return odgovor.data;
     })
     .catch((e)=>{console.error(e)})
 }
 
+async function getBySifra(sifra){
+    return await HttpService.get('/Smjer/' + sifra)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Ne postoji smjer!'}
+    })
+}
+
+async function obrisi(sifra) {
+    return await HttpService.delete('/Smjer/' + sifra)
+    .then((odgovor)=>{
+        //console.log(odgovor);
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        //console.log(e);
+        return {greska: true, poruka: 'Smjer se ne može obrisati!'}
+    })
+}
+
+async function dodaj(smjer) {
+    return await HttpService.post('/Smjer',smjer)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Smjer se ne može dodati!'}
+    })
+}
+
+async function promjena(sifra,smjer) {
+    return await HttpService.put('/Smjer/' + sifra,smjer)
+    .then((odgovor)=>{
+        return {greska: false, poruka: odgovor.data}
+    })
+    .catch((e)=>{
+        return {greska: true, poruka: 'Smjer se ne može promjeniti!'}
+    })
+}
+
 export default{
-    get
+    get,
+    getBySifra,
+    obrisi,
+    dodaj,
+    promjena
 }
