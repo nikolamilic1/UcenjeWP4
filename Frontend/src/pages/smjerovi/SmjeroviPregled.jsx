4e3810ae4e2c2ed0b5f1f762268160e397f6224c
Nikola <nikk.mil@gmail.com>
2024-09-19 18:55:48 +0200

    Update SmjeroviPregled.jsx

--- Frontend/src/pages/smjerovi/SmjeroviPregled.jsx
+++ Frontend/src/pages/smjerovi/SmjeroviPregled.jsx
@@ -2,37 +2,68 @@ import { Button, Container, Table } from "react-bootstrap";
 import SmjerService from "../../services/SmjerService";
 import { useEffect, useState } from "react";
 import { NumericFormat } from "react-number-format";
+import moment from "moment";
 import { GrValidate } from "react-icons/gr";
+import { RoutesNames } from "../../constants";
+import { Link, useNavigate } from "react-router-dom";
 
 
-export default function SmjeroviPregled() {
+
+export default function SmjeroviPregled(){
 
     const[smjerovi,setSmjerovi] = useState();
 
-
+    const navigate = useNavigate();
 
     async function dohvatiSmjerove() {
+
+        // zaustavi kod u Chrome consoli i tamo se može raditi debug
+        //debugger;
+        
         await SmjerService.get()
         .then((odgovor)=>{
-            setSmjerovi(odgovor)
+            //console.log(odgovor);
+            setSmjerovi(odgovor);
         })
-        .catch((e)=>console.log(e));
-    }
+        .catch((e)=>{console.log(e)});
 
+    }
 
     useEffect(()=>{
         dohvatiSmjerove();
     },[]);
 
+    function formatirajDatum(datum){
+        if(datum==null){
+            return 'Nije definirano';
+        }
+        return moment.utc(datum).format('DD. MM. YYYY.');
+    }
+
+    function vaucer(v){
+        if(v==null) return 'gray';
+        if(v) return 'green';
+        return 'red'
+    }
+
+    async function obrisiAsync(sifra) {
+        const odgovor = await SmjerService.obrisi(sifra);
+        //console.log(odgovor);
+        if(odgovor.greska){
+            alert(odgovor.poruka);
+            return;
+        }
+        dohvatiSmjerove();
+    }
+
+    function obrisi(sifra){
+        obrisiAsync(sifra);
+    }
 
 
-
-    function vaucer(v) {
-        if(v==null)
-    
-
-    return (
+    return(
         <Container>
+            <Link to={RoutesNames.SMJER_NOVI}>Dodaj novi smjer</Link>
             <Table striped bordered hover responsive>
                 <thead>
                     <tr>
@@ -48,44 +79,56 @@ export default function SmjeroviPregled() {
                     {smjerovi && smjerovi.map((smjer,index)=>(
                         <tr key={index}>
                             <td>{smjer.naziv}</td>
-                            <td
-                            
-                            >{smjer.trajanje}</td>
-                            <td className={smjer.cijena==null ? 'sredina' : 'desno'}>
-                                {smjer.cijena==null 
-                                ? 'Nije definirano' 
-                                :
-                                 <NumericFormat
-                                 value={smjer.cijena}
-                                 displayType={'text'}
-                                 thousandSeparator='.'
-                                 decimalSeparator=','
-                                 prefix={'€ '}
-                                 decimalScale={2}
-                                 fixedDecimalScale
-                                 />}
+                            <td className={smjer.trajanje==null ? 'sredina' : 'desno'}>
+                                {smjer.trajanje==null ? 'Nije definirano' : smjer.trajanje}
+                                
                                 </td>
-                            <td>{smjer.izvodiSeOd}</td>
-                            <td>
-                                <GrValidate
+                            <td className={smjer.cijena==null ? 'sredina' : 'desno'}>
+
+                                {smjer.cijena==null
+                                ? 'Nije definirano'
+                                : 
+                                <NumericFormat 
+                                value={smjer.cijena}
+                                displayType={'text'}
+                                thousandSeparator='.'
+                                decimalSeparator=','
+                                prefix={'€'}
+                                decimalScale={2}
+                                fixedDecimalScale
+                                />
+                            }
+
+                            </td>
+                            <td className={'sredina'}>
+                                {formatirajDatum(smjer.izvodiSeOd)}
+                            </td>
+                            <td className={'sredina'}>
+                                <GrValidate 
                                 size={30}
                                 color={vaucer(smjer.vaucer)}
                                 />
                             </td>
                             <td>
+                            <Button
+                                variant="primary"
+                                onClick={()=>navigate(`/smjerovi/${smjer.sifra}`)}>
+                                    Promjeni
+                                </Button>
+                                &nbsp;&nbsp;&nbsp;
                                 <Button
                                 variant="danger"
                                 onClick={()=>obrisi(smjer.sifra)}>
                                     Obriši
                                 </Button>
-                            </td>
 
-                    </tr>
-                ))}
+                               
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
-                </Table>
+            </Table>
         </Container>
     )
 
-}
 }
