72847a3b9e2cf63abf6386dca635f0fcacdbe781
Nikola <nikk.mil@gmail.com>
2024-09-19 18:55:32 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import NavBarAplikacija from './components/NavBarAplikacija';
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { RoutesNames } from './constants';
-import Pocetna from './pages/Pocetna';
-import SmjeroviPregled from './pages/smjerovi/SmjeroviPregled';
+import NavBarEdunova from './components/NavBarEdunova'
+import { Route, Routes } from 'react-router-dom'
+import { RoutesNames } from './constants'
+import Pocetna from './pages/Pocetna'
+import SmjeroviPregled from './pages/smjerovi/SmjeroviPregled'
+import SmjeroviDodaj from './pages/smjerovi/SmjeroviDodaj'
+import SmjeroviPromjena from './pages/smjerovi/SmjeroviPromjena'
+
 
 function App() {
+
   
-
-
   return (
     <>
-       <NavBarAplikacija/>
-       <Routes>
+      <NavBarEdunova />
+      <Routes>
         <Route path={RoutesNames.HOME} element={<Pocetna />} />
 
-
         <Route path={RoutesNames.SMJER_PREGLED} element={<SmjeroviPregled />} />
-       </Routes>
+        <Route path={RoutesNames.SMJER_NOVI} element={<SmjeroviDodaj />} />
+        <Route path={RoutesNames.SMJER_PROMJENA} element={<SmjeroviPromjena />} />
+      </Routes>
     </>
   )
 }
